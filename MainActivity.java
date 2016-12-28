216ead1688823793d37649aefca28822881de394
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-12-28 12:37:16 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,7 +3,6 @@ package com.example.atmen.flickrbrowser;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,51 +13,40 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate: starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        Log.d(TAG, "onCreate: ends");
     }
 
     @Override
     protected void onResume() {
-        Log.d(TAG, "onResume: starts");
         super.onResume();
         GetFlickrJsonData getFlickrJsonData = new GetFlickrJsonData(this, "https://api.flickr.com/services/feeds/photos_public.gne", "en-us", true);
-        getFlickrJsonData.executeOnSameThread("android, nougat");
-
-        Log.d(TAG, "onResume: ends");
+        getFlickrJsonData.execute("android, nougat");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        Log.d(TAG, "onCreateOptionsMenu() returned " + true);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Log.d(TAG, "onOptionsItemSelected() called");
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
             return true;
         }
-        Log.d(TAG, "onOptionsItemSelected() returned");
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onDataAvailable(List<Photo> data, DownLoadStatus status) {
         if (status == DownLoadStatus.OK) {
-            Log.d(TAG, "onDataAvailable: data is " + data);
+            // logic here
         } else {
-            Log.e(TAG, "onDataAvailable failed with status " + status);
+            // logic here
         }
     }
 
